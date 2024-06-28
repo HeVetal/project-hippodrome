78b613a985e4b5ca598b02a6addf7460a7f45920
HeVetal <heavyve@gmail.com>
2024-06-28 16:36:25 +0300

    add HippodromeTest

--- src/test/java/HippodromeTest.java
+++ src/test/java/HippodromeTest.java
@@ -1,5 +1,71 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class HippodromeTest {
 
+    @Test
+    public void constructor_illegalArgumentException_paramNull() {
+        assertThrows(IllegalArgumentException.class, () -> new Hippodrome(null));
+    }
+
+    @Test
+    public void constructor_message_paramNull() {
+        IllegalArgumentException thrown =
+                Assertions.assertThrows(IllegalArgumentException.class,
+                        () -> new Hippodrome(null));
+        assertEquals("Horses cannot be null.", thrown.getMessage());
+    }
+
+    @Test
+    public void constructor_illegalArgumentException_emptyList() {
+        assertThrows(IllegalArgumentException.class, () -> new Hippodrome(List.of()));
+    }
+
+    @Test
+    public void constructor_message_emptyList() {
+        var thrown = assertThrows(IllegalArgumentException.class, () -> new Hippodrome(List.of()));
+        assertEquals("Horses cannot be empty.", thrown.getMessage());
+    }
+
+    @Test
+    public void getHorses_returnHorses_() {
+//stream
+        List<Horse> list = new ArrayList<>();
+        for (int i = 0; i < 30; i++) {
+            list.add(new Horse("Zephyr " + i, i, i));
+        }
+        Hippodrome hippodrome = new Hippodrome(list);
+        assertEquals(list, hippodrome.getHorses());
+    }
+
+    @Test
+    public void testMove() {
+       //stream
+        List<Horse> list = new ArrayList<>();
+        for (int i = 0; i < 50; i++) {
+            list.add(Mockito.mock(Horse.class));
+        }
+        Hippodrome hippodrome = new Hippodrome(list);
+        hippodrome.move();
+        for (Horse horse : list) {
+            Mockito.verify(horse, Mockito.times(1)).move();//stream
+        }
+    }
+
+    @Test
+    public void testWinner() {
+        Horse horse = new Horse("Lobster", 1, 10);
+        Horse horse1 = new Horse("Pegasus", 2, 20);
+        Horse horse2 = new Horse("Cherry", 3, 30);
+        Hippodrome hippodrome = new Hippodrome(List.of(horse, horse1, horse2));
+        assertSame(horse2,hippodrome.getWinner());
+    }
 }
--- src/test/java/HorseTest.java
+++ src/test/java/HorseTest.java
@@ -4,27 +4,130 @@ import org.junit.jupiter.api.function.Executable;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.lang.reflect.Field;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class HorseTest {
-    //@Test(expected = IllegalArgumentException.class)
+    String name = "Blaze";
+    double speed = 1;
+    double distance = 2;
+
     @Test
-    public void ifNullShouldBeIllegalException() {
-        assertThrows(IllegalArgumentException.class, () -> new Horse(null, 1, 1));
+    public void constructor_illegalException_firstParamNull() {
+        assertThrows(IllegalArgumentException.class, () -> new Horse(null, speed, distance));
     }
 
     @Test
-    public void messageIfNull() {
+    public void constructor_message_firstParamNull() {
         IllegalArgumentException thrown =
                 Assertions.assertThrows(IllegalArgumentException.class,
-                        () -> new Horse(null,1,1),"Name cannot be null.");
+                        () -> new Horse(null, speed, distance));
         assertEquals("Name cannot be null.", thrown.getMessage());
     }
 
     @ParameterizedTest
-    @ValueSource(strings = {"","\\t"," "})
-    public void ifSpaseInConstructor(){
-       // assertThrows(IllegalArgumentException.class, () -> new Horse(, 1, 1));
+    @ValueSource(strings = {"", "\t", " ", "\n", "\f"})
+    public void constructor_illegalArgumentException_firstParamSpace(String symbol) {
+        assertThrows(IllegalArgumentException.class, () -> new Horse(symbol, speed, distance));
     }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", "\t", " ", "\n", "\f"})
+    public void constructor_message_firstParamSpace(String symbol) {
+        IllegalArgumentException thrown = assertThrows(IllegalArgumentException.class, () -> new Horse(symbol, speed, distance));
+        assertEquals("Name cannot be blank.", thrown.getMessage());
+    }
+
+    @Test
+    public void constructor_illegalArgumentException_secondParamOdd() {
+        assertThrows(IllegalArgumentException.class, () -> new Horse(name, -1, distance));
+    }
+
+    @Test
+    public void constructor_message_secondParamOdd() {
+        IllegalArgumentException thrown =
+                Assertions.assertThrows(IllegalArgumentException.class,
+                        () -> new Horse(name, -1, distance));
+        assertEquals("Speed cannot be negative.", thrown.getMessage());
+    }
+
+    @Test
+    public void constructor_illegalArgumentException_thirdParamMinus() {
+        assertThrows(IllegalArgumentException.class, () -> new Horse(name, speed, -1));
+    }
+
+    @Test
+    public void constructor_message_thirdParamMinus() {
+        IllegalArgumentException thrown =
+                Assertions.assertThrows(IllegalArgumentException.class,
+                        () -> new Horse(name, speed, -1));
+        assertEquals("Distance cannot be negative.", thrown.getMessage());
+    }
+
+    @Test
+    public void getNameReflection_returnName_() throws NoSuchFieldException, IllegalAccessException {
+        String expectedName = "Blaze";
+
+        Horse horse = new Horse(name, speed);
+        Field field = horse.getClass().getDeclaredField("name");
+        field.setAccessible(true);
+        String actualName = (String) field.get(horse);
+
+        assertEquals(expectedName, actualName);
+    }
+
+    @Test
+    public void testGetName_returnName_() throws NoSuchFieldException, IllegalAccessException {
+        Horse horse = new Horse(name, speed);
+
+        assertEquals(name, horse.getName());
+    }
+
+    @Test
+    public void getSpeedReflection_returnSpeed_() throws NoSuchFieldException, IllegalAccessException {
+        Horse horse = new Horse(name, speed);
+
+        Field field = horse.getClass().getDeclaredField("speed");
+        field.setAccessible(true);
+        double actualSpeed = (Double) field.get(horse);
+
+
+        assertEquals(speed, actualSpeed);
+    }
+
+    @Test
+    public void testGetSpeed_ShouldReturnSpeed_WhenArgumentIsConstructorSpeed_() {
+        Horse horse = new Horse(name, speed);
+
+        assertEquals(speed, horse.getSpeed());
+    }
+
+
+    @Test
+    public void getDistanceReflection_returnDistance() throws NoSuchFieldException, IllegalAccessException {
+        Horse horse = new Horse(name, speed, distance);
+
+        Field field = horse.getClass().getDeclaredField("distance");
+        field.setAccessible(true);
+        double actualDistance = (Double) field.get(horse);
+
+        assertEquals(distance, actualDistance);
+    }
+
+    @Test
+    public void getDistance_returnDistance_() {
+        Horse horse = new Horse(name, speed, distance);
+
+        assertEquals(distance, horse.getDistance());
+    }
+
+
+    @Test
+    public void getDistance_returnZero_twoParam() throws NoSuchFieldException, IllegalAccessException {
+        Horse horse = new Horse(name, speed);
+        assertEquals(0.0, horse.getDistance());
+    }
+
 
 }
